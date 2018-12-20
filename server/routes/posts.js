33721ee3ed43cbbf7ff33852f042bdfd9ae6a307
Jake Tompkins <term171013@gmail.com>
2018-12-20 21:25:18 +0800

    added posts index route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -10,4 +10,36 @@ const validatePost = require("./validations/posts");
 // Import user model
 const Post = require("../models/Post");
 
-// TODO: CRUD Posts
+// Get
+// Index all posts
+
+Router.get("/posts", (req, res) => {
+	const start = req.params.start || 0;
+
+	Post.find({}, (err, posts) => {
+		if (err) {
+			console.log(err);
+		} else {
+			posts = posts.slice(start, start + 5);
+			res.json({ posts });
+		}
+	});
+});
+
+// Protected routes
+
+// Post
+// Create new post
+
+Router.post(
+	"/posts",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		// Validate
+		const { errors, isValid } = validatePost(req.body);
+		if (!isValid) res.status(400).json(errors);
+
+		// TODO: Figure out how to parse and store the media
+		console.log(req.body);
+	}
+);
